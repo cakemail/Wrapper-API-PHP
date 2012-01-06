4e8ed5a454367c73b5c89bac9694a86d8999031e
Arthur Gouveia <arthur.gouveia@gmail.com>
2012-01-06 14:28:31 -0500

    Transfer

--- CakeAPI.class.php
+++ CakeAPI.class.php
@@ -2,39 +2,49 @@
 
 class CakeAPI {
     
-    private $apikey;
-    private $apiurl = 'https://api.wbsrvc.com';
-    
-    function __construct($apikey){
-        $this->apikey = $apikey;
-    }
-    
-    function call($url, $params) {
-        
-        $ch = curl_init();
-        
-        curl_setopt($ch, CURLOPT_URL, $this->apiurl . $url); 
-        curl_setopt($ch, CURLOPT_POST, true);
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('apikey:'.$this->apikey));
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
-        
-        $result = curl_exec($ch);
-        
-        if ($result === false) {
-            curl_close($ch);
-            throw new Exception('Curl error: ' . curl_error($ch));
-        } else {
-            if(!$result = json_decode($result)){
-                curl_close($ch);
-                throw new Exception('API Key Validation Error for ' . $this->apikey . '. Contact your administrator!');
-            } 
-            curl_close($ch);
-            if($result->status != 'success')
-                throw new Exception($result->data);
+  private $apikey;
+  private $apiurl = 'https://api.wbsrvc.com';
+
+  function __construct($apikey){
+    $this->apikey = $apikey;
+  }
+
+  function call($url, $params) {
+    try {
+
+      $ch = curl_init();
+
+      curl_setopt($ch, CURLOPT_URL, $this->apiurl . $url); 
+      curl_setopt($ch, CURLOPT_POST, true);
+      curl_setopt($ch, CURLOPT_HTTPHEADER, array('apikey:'.$this->apikey));
+      curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+      curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+      curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, true);
+
+      $result = curl_exec($ch);
+
+      if ($result === false) {
+
+        throw new Exception('Curl error: ' . curl_error($ch));
+
+      } else {
+
+        if (!$result = json_decode($result)) {
+          throw new Exception('API Key Validation Error for ' . $this->apikey . '. Contact your administrator!');
+        } 
+
+        if ($result->status != 'success') {
+          throw new Exception($result->data);
         }
-        
-        return $result->data;
+
+      }
+      
+      curl_close($ch);
+      return $result->data;
+
+    } catch (Exception $e) {
+      curl_close($ch);
+      return $e->getMessage();
     }
+  }
 }
